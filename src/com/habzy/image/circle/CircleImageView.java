2898d351cee043718f2aed89f30ac824412b1cb2
Habzy Huang <habzyhs@gmail.com>
2014-09-25 01:23:17 +0800

    First add empty ImageView for Circle Image View

---
+++ src/com/habzy/image/circle/CircleImageView.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ * Copyright habzyhs@gmail.com
+ * 
+ * Take reference from [RoundedImageView](https://github.com/habzy/RoundedImageView)
+ */
+package com.habzy.image.circle;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+
+public class CircleImageView extends ImageView {
+
+    public CircleImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }    
+    
+    public CircleImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        // TODO Auto-generated constructor stub
+    }
+
+}
